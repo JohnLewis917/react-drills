5472697c37465b3da2b5766040d53357d4e7b9ac
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-14 19:32:46 -0600

    not done - getting error

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Todo from './Todo'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      todoList: [],
+      newInput: '',
+    }
+  this.handleAdd = this.handleAdd.bind(this)
+
+  }
+  handleChange(value){
+    this.setState = this.handleChange({newInput: value})
+  }
+  handleAdd(){
+    this.setState({todoList: [...this.state.todoList, this.state.newInput], newInput: ""})
+  }
+
   render() {
+    let todoList = this.state.todoList.map((element, index) => {return <Todo key={index} task={element}/>})
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>My To-Do List</h1>
+        <div>
+          <input
+          value={this.state.input}
+          placeholder='Enter New Task'
+          onChange={e => this.handleChange(e.target.value)}
+          />
+          <button onClick={this.handleAdd}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {todoList}
+
       </div>
     );
   }
